0a4c15463dc50e8345e248c21cdf2623a3accf88
Pedro Mendes <hello@pedrofront.dev>
2023-07-10 01:35:47 -0300

    adding parsed metadata

--- src/app/[...ids]/page.tsx
+++ src/app/[...ids]/page.tsx
@@ -3,12 +3,20 @@ import Loader from "@/components/Loader";
 import Text from "@/components/Typography/text";
 import { BACKEND_URL } from "@/constants/env";
 import { cn } from "@/utils/className";
+import type { Metadata } from "next";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { toast } from "react-hot-toast";
+import urlMetadata from "url-metadata";
+
+type Props = {
+  params: { ids: string[] };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
 export default function Index({ params }: { params: { ids: string[] } }) {
   const router = useRouter();
+
   useEffect(() => {
     const fetchUrl = async () => {
       const hash = params.ids.slice(-1)[0];
@@ -21,6 +29,7 @@ export default function Index({ params }: { params: { ids: string[] } }) {
     };
     fetchUrl().catch(() => toast.error("Deu ruim, tente novamente."));
   }, []);
+
   return (
     <>
       <main
@@ -35,4 +44,111 @@ export default function Index({ params }: { params: { ids: string[] } }) {
       </main>
     </>
   );
+}
+
+function parseMetadata(fetchedMetadata: urlMetadata.Result): Metadata {
+  const title =
+    typeof fetchedMetadata?.title === "string"
+      ? fetchedMetadata?.title
+      : "Link encurtado peq.nu!";
+  const description =
+    typeof fetchedMetadata?.description === "string"
+      ? fetchedMetadata?.description
+      : "Encurtador de links simples e direto ao ponto :)";
+  const generator =
+    typeof fetchedMetadata?.generator === "string"
+      ? fetchedMetadata?.generator
+      : "peq.nu";
+  const keywords =
+    typeof fetchedMetadata?.keywords === "string"
+      ? fetchedMetadata?.keywords
+      : "peq.nu, encurtador, link, url";
+  const icons = {
+    icon:
+      typeof fetchedMetadata?.favicon === "string"
+        ? fetchedMetadata?.favicon
+        : "/favicon.ico",
+    apple:
+      typeof fetchedMetadata?.["apple-touch-icon"] === "string"
+        ? fetchedMetadata?.["apple-touch-icon"]
+        : "/favicon.ico",
+  };
+  const twitter = {
+    card:
+      typeof fetchedMetadata?.["twitter:card"] === "string"
+        ? fetchedMetadata?.["twitter:card"]
+        : "summary_large_image",
+    title:
+      typeof fetchedMetadata?.["twitter:title"] === "string"
+        ? fetchedMetadata?.["twitter:title"]
+        : title,
+    description:
+      typeof fetchedMetadata?.["twitter:description"] === "string"
+        ? fetchedMetadata?.["twitter:description"]
+        : "Encurtador de links simples e direto ao ponto :)",
+    images: [
+      typeof fetchedMetadata?.["twitter:image"] === "string"
+        ? fetchedMetadata?.["twitter:image"]
+        : `/api/og?title=${title}`,
+    ],
+  };
+  const openGraph = {
+    title:
+      typeof fetchedMetadata?.["og:title"] === "string"
+        ? fetchedMetadata?.["og:title"]
+        : title,
+    description:
+      typeof fetchedMetadata?.["og:description"] === "string"
+        ? fetchedMetadata?.["og:description"]
+        : "Encurtador de links simples e direto ao ponto :)",
+    images: [
+      {
+        url:
+          typeof fetchedMetadata?.["og:image"] === "string"
+            ? fetchedMetadata?.["og:image"]
+            : `/api/og?title=${title}`,
+        width: 1200,
+        height: 630,
+        alt: title,
+      },
+    ],
+    url:
+      typeof fetchedMetadata?.["og:url"] === "string"
+        ? fetchedMetadata?.["og:url"]
+        : "https://peq.nu",
+    siteName:
+      typeof fetchedMetadata?.["og:site_name"] === "string"
+        ? fetchedMetadata?.["og:site_name"]
+        : "peq.nu",
+  };
+
+  return {
+    metadataBase: new URL("https://peq.nu"),
+    title,
+    description,
+    openGraph,
+    generator,
+    keywords,
+    icons,
+    twitter,
+  };
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const hash = params.ids.slice(-1)[0];
+  if (!hash)
+    return {
+      title: "Link encurtado peq.nu!",
+      openGraph: {
+        images: ["/favicon.ico"],
+      },
+    };
+  //fetch metadata from final url
+  const finalUrl: string = await fetch(`${BACKEND_URL}${hash ?? ""}`)
+    .then((res) => res.json())
+    .then((res: { url: string }) => res.url);
+
+  const targetMetadata = await urlMetadata(finalUrl);
+
+  return parseMetadata(targetMetadata);
 }
---
+++ src/app/og/route.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ImageResponse } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const hasTitle = searchParams.has("title");
+    const title = hasTitle
+      ? searchParams.get("title")?.slice(0, 100)
+      : "My default title";
+    return new ImageResponse(
+      (
+        <div
+          tw="flex flex-col w-full h-full items-center justify-center"
+          style={{
+            backgroundImage:
+              "linear-gradient(to bottom, rgb(5,61,56),rgb(20,20,15))",
+          }}
+        >
+          <h1 tw="flex flex-col items-end justify-center">
+            <span
+              tw="text-[rgb(242,104,0)] text-8xl"
+              style={{
+                fontFamily: "Expletus Sans, serif",
+              }}
+            >
+              peq
+            </span>
+            <span tw="mt-[-22px] text-white text-8xl">
+              <span tw="text-[rgb(242,104,0)]">.</span>nu
+            </span>
+          </h1>
+          <h2
+            tw="text-5xl text-white uppercase"
+            style={{ fontFamily: "Expletus Sans, serif" }}
+          >
+            {title}
+          </h2>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      }
+    );
+  } catch (e: any) {
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
+}
