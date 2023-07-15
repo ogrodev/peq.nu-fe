e5ef14c5cf5f564f07d9f177d613307532c37bc2
Pedro Mendes <hello@pedrofront.dev>
2023-07-14 21:50:02 -0300

    adding 302 redirect status

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,14 @@ export default async function middleware(request: NextRequest) {
     const originalUrl: string = await fetch(`${BACKEND_URL}${hash}`)
       .then((res) => res.json())
       .then((res: { url: string }) => res.url);
-    return NextResponse.redirect(originalUrl);
+
+    const response = NextResponse.redirect(originalUrl, {
+      status: 302,
+      headers: {
+        "Cache-Control": "public, max-age=31536000, immutable",
+      },
+    });
+    return response;
   }
   return NextResponse.next();
 }
